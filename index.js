7f1730cf81b887a48be30048ec8388684366e383
Willian Souza <willian_aru_@hotmail.com>
2021-08-24 21:55:24 -0300

    Fix heroku deploy

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
-const Bot = require('./config');
-const puppeteer = require('puppeteer');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+// import getRandomQuote from './getRandomQuote';
 
-const getRandomQuote = async () => {
-	const browser = await puppeteer.launch();
-	const page = await browser.newPage();
-	await page.goto('https://lerolero.com/');
-	const lerolero = await page.evaluate(
-		() =>
-			document.getElementsByClassName('sentence sentence-exited')[0].innerHTML
-	);
-
-	Bot.post(
-		'statuses/update',
-		{
-			status: lerolero,
-		},
-		function (err, _data, _response) {
-			console.log(err);
-		}
-	);
-	await browser.close();
-};
+// import { parse } from 'node-html-parser';
+const getRandomQuote = require('./getRandomQuote');
 
 setInterval(async () => {
 	await getRandomQuote();
-}, 1000 * 60 * 60 * 2);
+	//}, 1000 * 60 * 60 * 2);
+}, 1000);
+
+express()
+	.use(express.static(path.join(__dirname, 'public')))
+	.set('views', path.join(__dirname, 'views'))
+	.set('view engine', 'ejs')
+	.get('/', (req, res) => res.render('pages/index'))
+	.listen(PORT, () => console.log(`Listening on ${PORT}`));
