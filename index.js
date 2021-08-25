1ab309f4cf9c2a75d9761e8ade65258cdac7e5bc
Willian Souza <willian_aru_@hotmail.com>
2021-08-25 01:06:19 -0300

    Remove await

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 const getRandomQuote = require('./getRandomQuote');
 
 // setInterval(async () => {
-await getRandomQuote();
+getRandomQuote();
 // }, 1000 * 60 * 60 * 2);
 
 express()
